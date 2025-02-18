c11fe014f14652fbafcd160acd7ca6396389d9c5
Sahas Amatya <133555714+Sahasatyy@users.noreply.github.com>
2025-02-18 10:32:13 +0545

    static files dirs added

--- user_management/settings.py
+++ user_management/settings.py
@@ -145,7 +145,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'users/static'),
+]
 
 
 # Default primary key field type
